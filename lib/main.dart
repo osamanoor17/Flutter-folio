a15855f91e7816490f01578af439f87e0dfdc6f6
Muhammad Osama Noor <100303780+Meizzosama@users.noreply.github.com>
2022-11-06 23:09:25 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:moxi_portfolio/projects.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import 'home.dart';
 
 void main() {
